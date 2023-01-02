7a3a80907f3394d9514a91e3585d5d3fd7d32696
lanniiee <lav250397@hotmail.com>
2023-01-02 15:12:24 +0000

    move component

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,37 +1,14 @@
-
-import { useEffect, useState } from "react";
 import "./Weather.scss";
 
+const Weather = ({weather}) => {
 
-const Weather = ({longitude, latitude}) => {
-
-    const [details, setDetails] = useState({});
-    const [icon, setIcon] = useState("");
-    const [locationName, setLocationName] = useState("");
-    const [lastUpdated, setLastUpdated] = useState("");
-
-
-    const getLocationDetails = async () => {
-        const url = `http://api.weatherapi.com/v1/current.json?key=c947b43abf2847c3987120210222012&q=${latitude},${longitude}&aqi=no`
-        const res = await fetch(url);
-        const data = await res.json();
-        setDetails(data)
-        setIcon(data.current.condition.icon)
-        setLocationName(data.location.name)
-        setLastUpdated(data.current.last_updated)
-    }
-
-    useEffect(() => {
-        getLocationDetails()
-    }, [latitude, longitude])
-
-    console.log(details)
+    console.log(weather)
 
     return (
         <div className="weather">
-            <h1 className="weather__name">{locationName}</h1>
-            <img className="weather__icon" src={icon} alt="icon" />
-            <p className="weather__date">{lastUpdated}</p>
+            <h1 className="weather__name">{weather.location.name}</h1>
+            <img className="weather__icon" src={weather.current.condition.icon} alt="icon" />
+            <p className="weather__date">{weather.current.last_updated}</p>
         </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,31 @@
 import "./Home.scss";
 import Weather from "../../components/Weather/Weather";
+import { useState, useEffect } from "react";
 
 const Home = ({longitude, latitude}) => {
+
+    const [weather, setWeather] = useState({});
+
+    const apiKey = "c947b43abf2847c3987120210222012"
+
+    const getLocationWeather = async () => {
+        const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}&aqi=no`
+        const res = await fetch(url);
+        const data = await res.json();
+        setWeather(data)
+    }
+
+    useEffect(() => {
+        getLocationWeather()
+    }, [latitude, longitude])
+
+    console.log(weather)
 
     return (
         <div className="home">
             <h1>Hello User</h1>
             {(latitude !== 0) ?
-                (<Weather longitude={longitude} latitude={latitude}/>)
+                (<Weather weather={weather} />)
                 : <div className="home__loading">Loading...</div>
             }
         </div>
